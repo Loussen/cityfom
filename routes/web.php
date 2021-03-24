d9d65cfc1cba303c253ece0aae1d59030d688561
Fuad Hasanli <fhesenli92@gmail.com>
2021-03-25 02:17:15 +0400

    Admin module created!

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('admin/login','Auth\AdminAuthController@getLogin')->name('adminLogin');
 Route::post('admin/login', 'Auth\AdminAuthController@postLogin')->name('adminLoginPost');
 Route::get('admin/logout', 'Auth\AdminAuthController@logout')->name('adminLogout');
@@ -20,8 +24,4 @@ Route::get('admin/logout', 'Auth\AdminAuthController@logout')->name('adminLogout
 Route::group(['prefix' => 'admin','middleware' => 'adminauth'], function () {
     // Admin Dashboard
     Route::get('dashboard','AdminController@dashboard')->name('dashboard');
-});
-
-Route::get('/', function () {
-    return view('welcome');
 });
